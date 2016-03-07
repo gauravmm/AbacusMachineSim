9134e7292700b00918b7637d111fc7e0a2f1dc16
Gaurav Manek <gaurav@gauravmanek.com>
2016-03-06 22:29:53 -0500

    FIX: Cleaned up syntax highlighter.

--- cmsyntax.js
+++ cmsyntax.js
@@ -7,20 +7,13 @@
 CodeMirror.defineSimpleMode("abacusmachine", {
   // The start state contains the rules that are intially used
   start: [
-    // The regex matches the token, the token property contains the type
     {regex: /"(?:[^\\]|\\.)*?"/, token: "string"},
-    // You can match multiple tokens at once. Note that the captured
-    // groups must span the whole string in this case
     {regex: /(function)(\s+)([a-z$][\w$]*)/,
      token: ["keyword", null, "variable"]},
-    // Rules are matched in the order in which they appear, so there is
-    // no ambiguity between this one and the one above
-    {regex: /((?:function|goto|where|next|is)\b)/,
-     token: "keyword"},
+    {regex: /((?:function|goto|where|next|is)\b)/, token: "keyword"},
     {regex: /\[[a-z0-9$]+\]/i, token: "variable-2"},
     {regex: /:[a-z0-9_$]+\w/i, token: "atom"},
-    {regex: /(?:\d+)/i,
-     token: "number"},
+    {regex: /(?:\d+)/i, token: "number"},
     // Inline comment:
     {regex: /\/\/.*/, token: "comment"},
     {regex: /\/(?:[^\\]|\\.)*?\//, token: "variable-3"},
@@ -36,10 +29,6 @@ CodeMirror.defineSimpleMode("abacusmachine", {
     {regex: /.*?\*\//, token: "comment", next: "start"},
     {regex: /.*/, token: "comment"}
   ],
-  // The meta property contains global information about the mode. It
-  // can contain properties like lineComment, which are supported by
-  // all modes, and also directives like dontIndentStates, which are
-  // specific to simple modes.
   meta: {
     dontIndentStates: ["comment"],
     lineComment: "//"
